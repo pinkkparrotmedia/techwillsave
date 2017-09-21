05dd35229fe05c5ac52fab78ddc341b0909663e0
Ronilo Yap <weprowd@gmail.com>
2017-09-21 22:57:22 +0600

    first commit

---
+++ page-mobile.php
@@ -0,0 +1,323 @@
+<?php
+
+/**
+ * Template Name: Mobile Template
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+ ?>
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title></title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="mobile-style.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+	<header>
+		<div class="wrapper">
+			<h2>Mover kit</h2>
+			<span class="tws-ages">ages 8 and above</span>
+			<span class="tws-price">£89.99</span>
+		</div>
+	</header>
+
+	<div class="tws-section1">
+		<div class="wrapper">
+			<div class="tws-quantity">
+				
+				<div class="tws-counter">
+					<h2>Quantity:  1</h2>
+					<a href="#" class="my-icons-plus"></a>
+					<a href="#" class="my-icons-negative"></a>
+				</div>
+				<a href="" class="tws-cart-btn">Add to cart</a>
+			</div>
+
+			<div class="tws-mid-tab">
+				<p>Kids code the Mover to react to their movement. Code it in infinite ways for hours of active fun! </p>
+				<a href="#">Free shipping <span class="my-icons-shape"></span></a>
+				<hr />
+				<a href="#">No quibble returns <span class="my-icons-page1"></span></a>
+			</div>
+
+			<div class="tws-icons-tab">
+				<ul>
+					<li>
+						<a href="">
+							<img src="images/feature_1.png" class="fimg1">
+							<span>Gets kids active and outdoors</span>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="images/feature_2.png" class="fimg2">
+							<span>Easy to follow manual</span>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="images/feature_3.png" class="fimg3">
+							<span>Dozens of projects online for all levels</span>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="images/feature_4.png" class="fimg4">
+							<span>Learn to code the easy way</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="tws-section2">
+		<div class="wrapper">
+			<iframe src="https://www.youtube.com/embed/JuTTbYIRhFs?rel=0&amp;showinfo=0" allowfullscreen></iframe>
+			<div class="tws-vid-content">
+				<h2>Fun, coding and 
+				creativity 
+				in one easy to use 
+				kit</h2>
+				<p>The Mover kit is the ideal way to introduce coding to kids. After all, play is the best way to learn!  Kids use the visual coding software to program the Mover to react to their movement and play in endless ways.</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="tws-section3">
+		<div class="wrapper">
+			<h2>YOUR CHILD’S FIRST STEP TO BECOMING A CODER</h2>
+
+			<ul>
+				<li>
+					<span>1</span>
+					<img src="images/how_1.gif">
+					<p>The Mover snaps together quickly and easily - no tools required!</p>
+				</li>
+				<li>
+					<span>2</span>
+					<img src="images/how_2.gif">
+					<p>The Mover snaps together quickly and easily - no tools required!</p>
+				</li>
+				<li>
+					<span>3</span>
+					<img src="images/how_3.gif">
+					<p>The Mover snaps together quickly and easily - no tools required!</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="tws-section4">
+		<div class="wrapper">
+			<h2>JOIN OUR GLOBAL MAKER COMMUNITY</h2>
+			<p>With the Mover Kit you will also get access to our free online platform for coding, making and inventing.</p>
+			
+			<ul>
+				<li>
+					<div class="tws-inner">
+						<span>projects made</span>
+						<h2>18,655</h2>
+					</div>
+				</li>
+				<li>
+					<div class="tws-inner">
+						<span>active makers</span>
+						<h2>110,892</h2>
+					</div>
+				</li>
+				<li>
+					<div class="tws-inner">
+						<span>used in</span>
+						<h2>183</h2>
+						<span>countries worldwide</span>
+					</div>
+				</li>
+			</ul>
+
+			<p>We made coding easy by designing our own simple drag and drop coding software.</p>
+
+			<img src="images/macbook.png">
+		</div>
+	</div>
+
+	<div class="tws-section5">
+		<div class="wrapper">
+			<h2>Keep learning and playing with new projects released every week!</h2>
+			<ul>
+				<li>
+					<img src="images/sect5-img1.jpg">
+					<div class="tws-inner">
+						<small>Easy</small>
+						<h3>Get started</h3>
+					</div>
+					<div class="tws-react">
+						<i class="my-icons-group2"></i>
+						<span class="tws-like">55</span>
+						<i class="my-icons-page1Copy3"></i>
+						<span class="tws-mins">10mins</span>
+					</div>
+				</li>
+
+				<li>
+					<img src="images/sect5-img2.jpg">
+					<div class="tws-inner">
+						<small>Easy</small>
+						<h3>Toothbrush</h3>
+					</div>
+					
+					<div class="tws-react">
+						<i class="my-icons-group2"></i>
+						<span class="tws-like">10</span>
+						<i class="my-icons-page1Copy3"></i>
+						<span class="tws-mins">10mins</span>
+					</div>
+				</li>
+
+				<li>
+					<img src="images/sect5-img1.jpg">
+					<div class="tws-inner">
+						<small>Easy</small>
+						<h3>Get started</h3>
+					</div>
+					<div class="tws-react">
+						<i class="my-icons-group2"></i>
+						<span class="tws-like">55</span>
+						<i class="my-icons-page1Copy3"></i>
+						<span class="tws-mins">10mins</span>
+					</div>
+				</li>
+			</ul>
+			<a href="https://make.techwillsaveus.com/mover-kit/" target="_blank">view more projects</a>
+		</div>
+	</div>
+
+	<div class="tws-section6">
+		<div class="wrapper">
+			<h2>SO WHAT 
+			AWESOMENESS 
+			ARE KIDS CREATING</h2>
+
+			<span class="tws-img-wrap">
+				<img src="images/social.jpg">
+				<img src="images/badge.png">
+			</span>
+			
+			<h3>Coding Kids At Fessenden School, Massachusetts</h3>
+			<p>@KimFogarty and the EDP crew gearing up for an afternoon of coding with their Mover Kits from @techwillsaveus 
+			#innovationstation #makered 
+			<span>@FessyiLab</span></p>
+		</div>
+	</div>
+
+	<div class="tws-section7">
+		<div class="wrapper">
+			<h2>For a world YET to be invented…</h2>
+			<p>Our mission is to inspire kids everywhere to use technology to create new things, solve life-changing problems, and invent their own future in a world where 65% of kids in school today will have a job that doesn’t exist yet.</p>
+			<div class="tws-inner">
+				<h3>With Mover kit 
+				kids learn</h3>
+				<ul>
+					<li>How to code</li>
+					<li>Problem solving</li>
+					<li>Creative thinking</li>
+					<li>How to customise tech</li>
+					<li>Make your own games…</li>
+				</ul>
+				<span>and it gets the kids 
+				active and playing 
+				outdoors.</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="tws-section8">
+		<div class="wrapper">
+			<h2>The making of award winning 
+			toys</h2>
+			<img src="images/awards.jpg">
+			<p>All of our kits are designed and crafted in London with the help of our fantastic Future Inventors Club testers. We're committed to creating kits that combine the wonder of play with making and coding.</p>
+			<p>Fun and experimentation are at the heart of all of our kits, but whisper it quietly, kids will also acquire a range of skills from creative thinking through to coding and electronics. Our kits are the perfect gifts for the Future Inventor in your life.</p>
+		</div>
+	</div>
+
+	<div class="tws-section9">
+		<div class="wrapper">
+			<h2>PICK YOUR KIT</h2>
+			<p>Get the kit that’s perfect 
+			for your kid!</p>
+			<ul>
+				<li>
+					<img src="images/cta_mover.jpg">
+					<div class="inner">
+						<h3>The mover kit</h3>
+						<span class="tws-price">£59.99</span>
+						<p>Kids code the Mover to react to their movement. Code it in infinite ways for hours of active fun! </p>
+						<a href="">Add to basket</a>
+					</div>
+				</li>
+
+				<li>
+					<img src="images/cta_sportslover.jpg">
+					<div class="inner">
+						<h3>SPORTS LOVER PACK</h3>
+						<span class="tws-price">£59.99</span>
+						<p>Kids code the Mover to react to their movement. Code it in infinite ways for hours of active fun!  </p>
+						<a href="">Add to basket</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<footer>
+		<div class="wrapper">
+			<h2>CUSTOMERS 
+			ALSO BOUGHT…</h2>
+			<ul>
+				<li>
+					<img src="images/cta_micromega.jpg">
+					<div class="inner">
+						<h3>Micro :mega pack</h3>
+						<span>£59.99 - Ages 11+</span>
+					</div>
+					
+				</li>
+				<li>
+					<img src="images/cta_gamer.jpg">
+					<div class="inner">
+						<h3>Micro :mega pack</h3>
+						<span>£59.99 - Ages 11+</span>
+					</div>
+				</li>
+				<li>
+					
+					<img src="images/cta_synth.jpg">
+					<div class="inner">
+						<h3>Micro :mega pack</h3>
+						<span>£59.99 - Ages 11+</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</footer>
+
+
+</body>
+</html>
